cf1543af8d8aa8bdbd758a3bd737f6765b50fc51
Res Yeg <shaqk702@gmail.com>
2018-05-24 15:07:39 -0700

    added form

--- djorg/bookmarks/models.py
+++ djorg/bookmarks/models.py
@@ -1,11 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 from uuid import uuid4
 
 # Create your models here.
 class Bookmark(models.Model):
- id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
- url = models.URLField('URL', unique=True)
- name = models.CharField(max_length=200)
- notes = models.TextField(blank=True)
- created_at = models.DateTimeField(auto_now_add=True)
- last_modified = models.DateTimeField(auto_now=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    url = models.URLField('URL', unique=True)
+    name = models.CharField(max_length=200)
+    notes = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+class PersonalBookmark(Bookmark):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    # id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    # url = models.URLField('URL', unique=True)
+    # name = models.CharField(max_length=200)
+    # notes = models.TextField(blank=True)
+    # created_at = models.DateTimeField(auto_now_add=True)
+    # last_modified = models.DateTimeField(auto_now=True)
